9d4fd541025d73ccb51f07b51cc557477687f847
hardcoder0620 <thehardcoder0620@gmail.com>
2024-12-05 23:14:28 +0530

    customer crud started

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stockRouter = require('./routes/stocks');
+var customerRouter = require('./routes/customers');
 
 var app = express();
 
@@ -14,6 +15,8 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
+var cors = require("cors");
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +28,8 @@ app.use('/users', usersRouter);
 
 // stocks api routes 
 app.use('/stocks', stockRouter);
+// customers routes 
+app.use('/customers', customerRouter);
 
 
 
---
+++ routes/customers.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+const models = require("../models");
+const { Op } = require('sequelize');
+
+/* Create stock. */
+router.post('/create', function(req, res) {
+
+ 
+
+    let customer = {
+        name: req.body.name,
+        address: req.body.address,
+        mobile: req.body.mobile,
+        email: req.body.email,
+        pendingAmount: req.body.pendingAmount,
+    }
+
+    models.Customer.create(customer).then(response=>{
+        res.status(200).json({
+            "status":true,
+            "msg":"customer created successfully",
+            "stock":customer
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            "status":false,
+            "msg":"something went wrong",
+            "errors":err
+        })
+    })
+
+  
+});
+
+
+/* stock list. */
+router.get('/', function(req, res) {
+
+    const {page = 1,limit = 10,search = ''} = req.query
+
+    let query = {}
+    if(search){
+        query.itemName = {[Op.substring] : search}
+    }
+
+
+    models.Stock.findAndCountAll({
+        where : query,
+        limit : Number(limit),
+        offset : (page - 1) * limit
+       
+    }).then(response=>{
+        res.status(200).json({
+            "status":true,
+            "msg":"stock found!!",
+            "stock":response.rows,
+            "pagination":{
+                "curPage": Number(page),
+                "totalPages": Math.ceil(response.count / Number(limit)) ,
+                "total" : response.count
+            }
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            "status":false,
+            "msg":"something went wrong",
+            "errors":err
+        })
+    })
+  
+});
+
+
+/* Create stock. */
+router.post('/update', function(req, res) {
+
+    const {id} = req.query
+
+    let stock = {
+        itemName: req.body.itemName,
+        itemType: req.body.itemType,
+        itemGroup: req.body.itemGroup,
+        itemWeight: req.body.itemWeight,
+        stoneWeight: req.body.stoneWeight,
+        finalWeight: req.body.finalWeight,
+        stock: req.body.stock,
+        sellBy: req.body.sellBy,
+        uom: req.body.uom
+    }
+
+    models.Stock.update(stock,{where : {id : id}}).then(response=>{
+        res.status(200).json({
+            "status":true,
+            "msg":"stock updated successfully",
+            "stock":stock
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            "status":false,
+            "msg":"something went wrong",
+            "errors":err
+        })
+    })
+
+  
+});
+
+/* gte single  stock. */
+router.get('/details', function(req, res) {
+
+    const {id} = req.query
+
+    
+
+    models.Stock.findByPk(id).then(response=>{
+
+        if(!response){
+            res.status(500).json({
+                "status":false,
+                "msg":"Not found"
+            })
+        }else{
+            res.status(200).json({
+                "status":true,
+                "msg":"stock updated successfully",
+                "stock":response
+            })
+        }
+
+       
+    }).catch(err=>{
+        res.status(500).json({
+            "status":false,
+            "msg":"something went wrong",
+            "errors":err
+        })
+    })
+
+  
+});
+
+
+module.exports = router;
