1712b9bf4ef84c361d959ee387da8f9b30cd1c0e
asesa-rushikesh-pingle <rushikesh.pingle@asesadigital.com>
2024-12-12 16:41:03 +0530

    pdf generated successfully

--- routes/bills.js
+++ routes/bills.js
@@ -27,8 +27,404 @@ router.post('/create', checkauth, async function (req, res) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    let htmll = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+    <style>
+        *{
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
+        .invoice {
+
+            border: 3px double rgb(124, 124, 124);
+            width: 540px;
+            height: 750px;
+            padding: 10px;
+        }
+        .mainHead{
+            margin-top: 10px;
+            text-align: center;
+            font-size: 32px;
+            font-weight: 600;
+            color: rgb(255, 115, 0);
+        }
+        .nameeandMob{
+            text-align: center;
+            margin-top: 5px;
+            font-size: 15px;
+            font-weight: 600;
+            color: rgb(0, 0, 0);
+        }
+        .bbt{
+            border-bottom: 1px solid rgb(124, 124, 124);
+            padding-bottom: 15px;
+        }
+        .address{
+            text-align: center;
+           padding-top: 5px;
+           padding-bottom: 5px;
+            font-size: 14px;
+            font-weight: 500;
+            color: rgb(0, 0, 0);
+            border-bottom: 1px solid rgb(124, 124, 124);
+        }
+        .bandDate{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: 5px;
+        }
+        .bilNo{
+            display: flex;
+            align-items: center;
+            column-gap: 10px;
+        }
+        .tit{
+            font-weight: 600;
+            font-size: 14px;
+            color: black;
+        }
+        .val{
+            font-weight: 500;
+            font-size: 14px;
+            color: black;
+        }
+        .tabllee{
+            border-collapse: collapse;
+            margin-top: 15px;
+            width: 100%;
+            /* background: rgb(243, 243, 243); */
+            background: rgb(255, 240, 227);
+        }
+        .tabllee thead tr th{
+            padding: 5px 2px;
+        }
+        .tabllee tbody tr td,.tabllee tbody tr th{
+            padding: 3px 2px;
+        }
+      
+        .tabllee tbody{
+            /* background: rgb(243, 243, 243); */
+        }
+        .tabllee tr{
+            text-align: left;
+            font-size: 12px;
+            
+        }
+        .totalTable{
+            width: 150px;
+            /* float: right; */
+            border-collapse: collapse;
+            background: rgb(255, 240, 227);
+            margin-left: auto;
+        }
+        .totalTable th{
+            padding: 2px 5px;
+
+
+        }
+        .bgTotall{
+            background: rgb(255, 143, 46);
+        }
+
+        .thanks{
+            text-align: center;
+            font-size: 18px;
+            font-style: italic;
+            font-weight: 600;
+            color: black;
+            margin-top: 30px;
+
+        }
+        
+    </style>
+</head>
+
+<body>
+
+    <div class="invoice">
+        <div class="bbt">
+            <div class="mainHead">
+                Shree Arihant Jewellers
+            </div>
+            <div class="nameeandMob">
+               Sagar Babaso Kandgave, MOB : 8805477394
+            </div>
+        </div>
+        <div class="address">
+            Koshti Galli, Near Society Shop, Bhingare Building, Kagal
+        </div>
+        <div class="bandDate">
+            <div class="bilNo">
+                <div class="tit">
+                    Bill No:
+                </div>
+                <div class="val">
+                    2347
+                </div>
+            </div>
+            <div class="bilNo">
+                <div class="tit">
+                    Date:
+                </div>
+                <div class="val">
+                    2023/12/03
+                </div>
+            </div>
+        </div>
+        <div class="bandDate">
+            <div class="bilNo">
+                <div class="tit">
+                    Name:
+                </div>
+                <div class="val">
+                    Customer Name here
+                </div>
+            </div>
+           
+        </div>
+        <div class="bandDate">
+            <div class="bilNo">
+                <div class="tit">
+                    Mobile:
+                </div>
+                <div class="val">
+                    Customer mobile
+                </div>
+            </div>
+           
+        </div>
+
+        <table  border="1" class="tabllee">
+            <thead >
+                <tr>
+                    <th>
+                        Item Name
+                    </th>
+                    <th>
+                         Type
+                    </th>
+                    <th>
+                        Rate
+                    </th>
+                    <th>
+                        Weight
+                    </th>
+                    <th>
+                        Qty
+                    </th>
+                    <th>
+                        Making
+                    </th>
+                    <th>
+                        Total
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        Item Name Hhfsbdfhb sjfd sdfhbs dfsdhf bsd
+                    </td>
+                    <td>
+                        Gold
+                    </td>
+                    <td>
+                        2332
+                    </td>
+                    <td>
+                        23
+                    </td>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        300
+                    </td>
+                    <th>
+                        249
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        Item Name Hhfsbdfhb sjfd sdfhbs dfsdhf bsd
+                    </td>
+                    <td>
+                        Gold
+                    </td>
+                    <td>
+                        2332
+                    </td>
+                    <td>
+                        23
+                    </td>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        300
+                    </td>
+                    <th>
+                        249
+                    </th>
+                </tr>
+                
+
+            </tbody>
+        </table>
+
+        <table  border="1" class="tabllee">
+            <thead >
+                <tr>
+                    <th>
+                        Old Item Name
+                    </th>
+                    <th>
+                         Type
+                    </th>
+                    <th>
+                        Rate
+                    </th>
+                    <th>
+                        Weight
+                    </th>
+                    <th>
+                        Qty
+                    </th>
+                  
+                    <th>
+                        Total
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        Item Name Hhfsbdfhb sjfd sdfhbs dfsdhf bsd
+                    </td>
+                    <td>
+                        Gold
+                    </td>
+                    <td>
+                        2332
+                    </td>
+                    <td>
+                        23
+                    </td>
+                    <td>
+                        1
+                    </td>
+                  
+                    <th>
+                        249
+                    </th>
+                </tr>
+             
+                
+
+            </tbody>
+        </table>
+
+ 
+            <table  border="1"  class="tabllee totalTable">
+            
+                <tbody>
+                    <tr >
+                       
+                        <th>
+                            Pending
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr>
+                       
+                        <th>
+                            Sub Total
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr>
+                       
+                        <th>
+                            Old Total
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr>
+                       
+                        <th>
+                            Discount
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr class="bgTotall">
+                       
+                        <th>
+                            Final Total
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr>
+                       
+                        <th>
+                            Amount Paid
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                    <tr>
+                       
+                        <th>
+                            Remaining
+                        </th>
+                      
+                        <th>
+                            249
+                        </th>
+                    </tr>
+                 
+                    
+    
+                </tbody>
+            </table>
+     
+
+        
+
+        <div class="thanks">
+            Thank You...
+        </div>
+            
+    </div>
+
+</body>
+
+</html>`
+
     // Set the HTML content
-    await page.setContent("<div style='color:red;border:1px solid red;width:540px;height:750px;' >Test H2</div>", { waitUntil: 'load' });
+    await page.setContent(htmll, { waitUntil: 'load' });
 
     // Define the file path to save the PDF
     const filePath = path.join( 'pdfs', `${Date.now() + "invoice"}.pdf`);
