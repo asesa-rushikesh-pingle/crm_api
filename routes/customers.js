3e159ba59cb23b63ac5b15c5315b703aa8f8f929
asesa-rushikesh-pingle <rushikesh.pingle@asesadigital.com>
2024-12-26 15:14:54 +0530

    user details issue

--- routes/customers.js
+++ routes/customers.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const models = require("../models");
-const { Op } = require('sequelize');
-const checkAuth =  require("../middleware/auth")
+const { Op, where } = require('sequelize');
+const checkAuth = require("../middleware/auth")
 
 /* Create stock. */
-router.post('/create' ,checkAuth, function(req, res) {
+router.post('/create', checkAuth, function (req, res) {
 
- 
+
 
     let customer = {
         name: req.body.name,
@@ -17,67 +17,67 @@ router.post('/create' ,checkAuth, function(req, res) {
         pendingAmount: req.body.pendingAmount,
     }
 
-    models.Customer.create(customer).then(response=>{
+    models.Customer.create(customer).then(response => {
         res.status(200).json({
-            "status":true,
-            "msg":"customer created successfully",
-            "stock":customer
+            "status": true,
+            "msg": "customer created successfully",
+            "stock": customer
         })
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json({
-            "status":false,
-            "msg":"something went wrong",
-            "errors":err
+            "status": false,
+            "msg": "something went wrong",
+            "errors": err
         })
     })
 
-  
+
 });
 
 
 /* stock list. */
-router.get('/',checkAuth, function(req, res) {
+router.get('/', checkAuth, function (req, res) {
 
-    const {page = 1,limit = 10,search = ''} = req.query
+    const { page = 1, limit = 10, search = '' } = req.query
 
     let query = {}
-    if(search){
-        query.name = {[Op.substring] : search}
+    if (search) {
+        query.name = { [Op.substring]: search }
     }
 
-    
+
 
     models.Customer.findAndCountAll({
-        where : query,
-        limit : Number(limit),
-        offset : (page - 1) * limit
-       
-    }).then(response=>{
+        where: query,
+        limit: Number(limit),
+        offset: (page - 1) * limit
+
+    }).then(response => {
         res.status(200).json({
-            "status":true,
-            "msg":"customers found!!",
-            "customers":response.rows,
-            "pagination":{
+            "status": true,
+            "msg": "customers found!!",
+            "customers": response.rows,
+            "pagination": {
                 "curPage": Number(page),
-                "totalPages": Math.ceil(response.count / Number(limit)) , 
-                "total" : response.count
+                "totalPages": Math.ceil(response.count / Number(limit)),
+                "total": response.count
             }
         })
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json({
-            "status":false,
-            "msg":"something went wrong",
-            "errors":err
+            "status": false,
+            "msg": "something went wrong",
+            "errors": err
         })
     })
-  
+
 });
 
 
 /* Create stock. */
-router.post('/update',checkAuth, function(req, res) {
+router.post('/update', checkAuth, function (req, res) {
 
-    const {id} = req.query
+    const { id } = req.query
 
     let customer = {
         name: req.body.name,
@@ -87,57 +87,64 @@ router.post('/update',checkAuth, function(req, res) {
         pendingAmount: req.body.pendingAmount,
     }
 
-    models.Customer.update(customer,{where : {id : id}}).then(response=>{
+    models.Customer.update(customer, { where: { id: id } }).then(response => {
         res.status(200).json({
-            "status":true,
-            "msg":"customer updated successfully",
-            "stock":customer
+            "status": true,
+            "msg": "customer updated successfully",
+            "stock": customer
         })
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json({
-            "status":false,
-            "msg":"something went wrong",
-            "errors":err
+            "status": false,
+            "msg": "something went wrong",
+            "errors": err
         })
     })
 
-  
+
 });
 
 /* gte single  stock. */
-router.get('/details',checkAuth, function(req, res) {
+router.get('/details', checkAuth, async function (req, res) {
 
-    const {id} = req.query
+    const { id } = req.query
 
-    
+    try {
+        let customerObj = await models.Customer.findByPk(id)
+        if (customerObj) {
+            console.log("customerObj",customerObj)
 
-    models.Customer.findByPk(id),{
-        include : [models.Bill]
-    }.then(response=>{
-
-        if(!response){
-            res.status(500).json({
-                "status":false,
-                "msg":"Not found"
+            let ordersArr  = await models.Bill.findAll({
+                where : {customerId : customerObj.id}
             })
-        }else{
+
+
+
+            customerObj.dataValues.orders = ordersArr
             res.status(200).json({
-                "status":true,
-                "msg":"Customer Found successfully",
-                "customer":response
+                "status": false,
+                "msg": "User found successfully",
+                "customer": customerObj.dataValues
+            })
+        } else {
+            res.status(500).json({
+                "status": false,
+                "msg": "user not found"
             })
         }
 
-       
-    }).catch(err=>{
-        res.status(500).json({
-            "status":false,
-            "msg":"something went wrong",
-            "errors":err
-        })
-    })
 
-  
+
+    } catch (error) {
+        res.status(500).json({
+            "status": false,
+            "msg": "something went wrong",
+            "errors": error
+        })
+    }
+
+
+
 });
 
 
